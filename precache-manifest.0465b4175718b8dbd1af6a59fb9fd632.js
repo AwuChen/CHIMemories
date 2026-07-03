336a074e94755f7ea3635d5ad9c94bede9215b51
Awu Chen <awwu@Awus-MacBook-Pro.local>
2026-07-03 16:06:21 +0800

    Updates

---
+++ precache-manifest.0465b4175718b8dbd1af6a59fb9fd632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793622c754b64c26dd86d2b5d36ec2c9",
+    "url": "/CHIMemories/index.html"
+  },
+  {
+    "revision": "6e6bfcf774e9d8985fc7",
+    "url": "/CHIMemories/static/css/main.ead59979.chunk.css"
+  },
+  {
+    "revision": "65a23980472fb6932a95",
+    "url": "/CHIMemories/static/js/2.9ec64b76.chunk.js"
+  },
+  {
+    "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
+    "url": "/CHIMemories/static/js/2.9ec64b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6bfcf774e9d8985fc7",
+    "url": "/CHIMemories/static/js/main.963d5c56.chunk.js"
+  },
+  {
+    "revision": "3ddc818cce9181dd5284",
+    "url": "/CHIMemories/static/js/runtime-main.e40e7722.js"
+  }
+]);
